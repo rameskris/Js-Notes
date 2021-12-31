8d2c1d93c15cfb5ce852f1502fb3992e1b568589
rameskris <redmiblack2@gmail.com>
2021-12-31 17:50:12 +0530

    third commt

--- jstodo-project/app.js
+++ jstodo-project/app.js
@@ -1,2 +1,29 @@
-const addTodo=document.querySelector(".add")
+const addForm=document.querySelector(".add")
+const todos=document.querySelector(".todos")
 
+generateTodos=todo=>{
+    todos.innerHTML+=`
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+    <span>${todo}</span>
+    <i class="far fa-trash-alt delete"></i>
+  </li>`
+}
+
+addForm.addEventListener('submit',e=>{
+    e.preventDefault()
+    //trim the sapces
+    const todo=addForm.add.value.trim();
+    if(todo.length){
+        generateTodos(todo)
+        addForm.reset()
+        
+
+    }   
+
+})
+//delete todos
+todos.addEventListener('click',e=>{
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.remove()
+    }
+})
